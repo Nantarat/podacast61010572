ae80f14d2d0f6c0d7c4f7422bf2fdf5a336841c8
Nantarat <61010572/2kmitl.ac.th>
2020-10-05 22:31:14 +0700

    final project

---
+++ stock/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+# Create your views here.
+def index(request):
+    products = Product.objects.all()
+    return render (request, 'frontend/index.html',{'products': products})
+def about(request):
+    return render (request, 'frontend/about.html')
+def contact(request):
+    return render (request, 'frontend/contact.html')
